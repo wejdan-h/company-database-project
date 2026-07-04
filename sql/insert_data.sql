8968dd623e5797924c2d14326e1c1c770d59a812
Wejdan Hakami <wejdan.hakami.ha@gmail.com>
2026-07-04 18:05:05 +0300

    Add initial data to database tables
    Inserted multiple records into Company, Manager, Department, Employee, Product, Order1, and Customer tables.

---
+++ sql/insert_data.sql
@@ -0,0 +1,27 @@
+INSERT INTO Company VALUES (1, 'XYZ Corp', '456 Elm St');
+INSERT INTO Company VALUES (2, 'ahmad', '456 Elm St');
+INSERT INTO Company VALUES (3, 'VTEVIC', '754E CR');
+
+INSERT INTO Manager VALUES (1, 'ali', '053827616', 10200.20, 1);
+INSERT INTO Manager VALUES (2, 'omar', '054725468', 10666.40, 2);
+INSERT INTO Manager VALUES (3, 'Hasan', '053756382', 15600.40, 3);
+
+INSERT INTO Department VALUES (1, 'fatemah', 1);
+INSERT INTO Department VALUES (2, 'amjad', 2);
+INSERT INTO Department VALUES (3, 'Atheer', 2);
+
+INSERT INTO Employee VALUES (1, 'ali', '0538542675', 1300.00, 1);
+INSERT INTO Employee VALUES (2, 'mohammed', '0538576475', 1300.00, 2);
+INSERT INTO Employee VALUES (3, 'sam', '0538567843', 1300.00, 3);
+
+INSERT INTO Product VALUES (1, 'Laptop', 1000.99, 1);
+INSERT INTO Product VALUES (2, 'dress', 500.99, 2);
+INSERT INTO Product VALUES (3, 'Milk', 8.99, 3);
+
+INSERT INTO Order1 VALUES (1, 1, 1, DATE '2023-02-20');
+INSERT INTO Order1 VALUES (2, 2, 2, DATE '2020-02-23');
+INSERT INTO Order1 VALUES (3, 3, 3, DATE '2012-02-21');
+
+INSERT INTO Customer VALUES (1, 'Mohammed', '336 Elm St', 'm@mail.com', 1);
+INSERT INTO Customer VALUES (2, 'Ahmad', '456 Elm St', 'a@mail.com', 2);
+INSERT INTO Customer VALUES (3, 'Ali', '885 Elm St', 'l@mail.com', 3);
